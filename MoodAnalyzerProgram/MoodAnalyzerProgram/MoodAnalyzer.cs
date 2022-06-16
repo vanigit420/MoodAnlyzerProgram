a021efa52337792b0d57ad4952658b74920a38e2
shivani korwan <shivanikorwan63232@gmail.com>
2022-06-16 09:45:44 +0530

    creating method to analyse mood

---
+++ MoodAnalyzerProgram/MoodAnalyzerProgram/MoodAnalyzer.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MoodAnalyserProblem
+{
+    public class MoodAnalyser
+    {
+        string message;
+        public MoodAnalyser(string message)
+        {
+            this.message = message;
+        }
+        //method to Analyse mood 
+        public string AnalyseMood()
+        {
+            message = message.ToLower();
+            if (message.Contains("sad"))
+            {
+                return "sad";
+            }
+            else
+            {
+                return "happy";
+            }
+        }
+    }
+}
